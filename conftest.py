07f08387f33b4c7d79b3acc64a611ade2840dba7
Khushbu Mehta <khushbumehta@Khushbus-Laptop.local>
2026-05-26 16:25:30 -0700

    Set headless=True for Jenkins compatibility

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from playwright.sync_api import sync_playwright
 @pytest.fixture(scope="session")
 def browser():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False) # headless false so that you can see the browser
+        browser = p.chromium.launch(headless=True) # headless false so that you can see the browser
         yield browser
         browser.close()
 
